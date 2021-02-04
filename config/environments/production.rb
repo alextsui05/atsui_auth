e12ce67998ff71cf4d488295705b9fe7866bf0fb
Alex Tsui <alextsui@pm.me>
2021-02-04 06:44:39 +0000

    Revert ":ok_hand: Tweak production mailer host"
    This reverts commit 3569d9f9602fef772d99b73e6e85cb7299c31c76.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = {
-    host: 'atsui.dev',
+    host: 'x.atsui.dev',
     protocol: 'https',
   }
 
